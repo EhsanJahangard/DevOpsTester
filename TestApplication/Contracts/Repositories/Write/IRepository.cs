f1964ccb72e4ca1a13e5d242596ada26704fe581
Ehsan Jahangard <ehsan.jahangard.2014@gmail.com>
2024-07-23 13:31:04 +0330

    approve TestApplication Layer

---
+++ TestApplication/Contracts/Repositories/Write/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication.Contracts.Repositories.Write
+{
+    public interface IRepository<T> where T : class
+    {
+
+    }
+}
---
+++ TestPersistence/Configs/LevelConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDomain.Models;
+
+namespace TestPersistence.Configs
+{
+    public class LevelConfig : IEntityTypeConfiguration<Level>
+    {
+        public void Configure(EntityTypeBuilder<Level> builder)
+        {
+            builder.Property(r => r.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasDefaultValueSql("NEWID()");
+
+        }
+    }
+}
