0a4eb89058a63821f739f9a12ac989c2c7b97f69
Ehsan Jahangard <ehsan.jahangard.2014@gmail.com>
2024-08-27 13:37:50 +0330

    Compele Models Of Domain Layer for Identity

---
+++ IdentityDomain/Models/Role.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityDomain.Models;
+
+public class Role : IdentityRole
+{
+    public Role(string name, string description, string sender) : base(name)
+    {
+        Description = description;
+        ConcurrencyStamp = Guid.NewGuid().ToString();
+        Creator = sender;
+        CreateDate = DateTime.Now;
+        Editor = sender;
+        EditDate = DateTime.Now;
+        Permissions = [];
+
+    }
+
+    public string Description { get; private set; }
+    public string Creator { get; private set; }
+    public DateTime CreateDate { get; private set; }
+    public string Editor { get; private set; }
+    public DateTime EditDate { get; private set; }
+
+    public List<Permission> Permissions { get; private set; }
+
+    public void Update(Role role)
+    {
+        Name = role.Name;
+        Description = role.Description;
+        Editor = role.Editor;
+        EditDate = DateTime.Now;
+    }
+
+    public void AddPermissions(List<Permission> permissions)
+    {
+        Permissions = [.. permissions];
+    }
+
+    private Role() { }
+}
--- IdentityDomain/Models/User.cs
+++ IdentityDomain/Models/User.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
-namespace IdentityDomain.Models
+namespace IdentityDomain.Models;
+
+public class User : IdentityUser
 {
-    internal class User
+    public User(string firstName, string lastName, string email, string username, string mobile, string sender) : base(username)
     {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Mobile = mobile;
+        Creator = sender;
+        CreateDate = DateTime.Now;
+        Editor = sender;
+        EditDate = DateTime.Now;
     }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Mobile { get; private set; }
+    public int? City { get; private set; } = 0;
+    public int? Country { get; private set; } = 0;
+    public string? PostalCode { get; private set; } = "0000000000";
+    public string Creator { get; private set; }
+    public DateTime CreateDate { get; private set; }
+    public string Editor { get; set; }
+    public DateTime EditDate { get; set; }
+
+    public void Update(User user)
+    {
+        FirstName = user.FirstName;
+        LastName = user.LastName;
+        Email = user.Email;
+        Mobile = user.Mobile;
+        Editor = user.Editor;
+        EditDate = DateTime.Now;
+    }
+
+    private User() { }
+
 }
